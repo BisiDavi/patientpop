84e69d55d725260e718bdaffb0f94db2d9d56e99
BisiDavi <oludavidconnect@gmail.com>
2021-08-17 10:49:54 +0100

    styles updated

--- components/notify.tsx
+++ components/notify.tsx
@@ -2,12 +2,21 @@ import { Button } from "@components/.";
 
 export default function Notify() {
   return (
-    <div className="flex items-center py-4 justify-center fixed bottom-0 bg-white w-full">
-      <p className="text-xl font-semibold text-gray-500 mb-0">
-        Instantly see how you compare to other practices in your local area and
-        specialty.
-      </p>
-      <Button className="py-1 mx-8" text="Compare" />
-    </div>
+    <>
+      <div className="notify flex items-center py-4 justify-center fixed bottom-0 bg-white w-full">
+        <p className="text-xl font-semibold text-gray-500 mb-0">
+          Instantly see how you compare to other practices in your local area
+          and specialty.
+        </p>
+        <Button className="py-1 mx-8" text="Compare" />
+      </div>
+      <style jsx>
+        {`
+          .notify.flex {
+            box-shadow: 0 -3px 3px -3px rgba(0, 0, 0, 0.25);
+          }
+        `}
+      </style>
+    </>
   );
 }
